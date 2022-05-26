eeed3caf9d9a21a22ba684ec254df10c4dc26177
LZY_Shrio <170280176@qq.com>
2022-05-26 20:30:48 +0800

    blog详情页

--- src/pages/hello/[name].vue
+++ src/pages/hello/[name].vue
@@ -2,10 +2,20 @@
 const props = defineProps({ name: String })
 </script>
 <template>
-  <div class="my-5">
-    <div class="mb-5 font-bold">
-      😀Hello,<span class="text-3xl ml-2">{{ props.name }}</span>
+  <div class="wrap">
+    <TopHeader size="true" />
+    <div class="my-5">
+      <div class="mb-5 font-bold">
+        <span class="text-3xl ml-2">{{ props.name }}</span>
+      </div>
     </div>
-    <router-link to="/">back</router-link>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.wrap {
+  max-width: 42rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
