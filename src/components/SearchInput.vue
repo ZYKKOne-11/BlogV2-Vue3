13635f6392ef99d3f72aa2f7061d21f03acc608b
LZY_Shrio <170280176@qq.com>
2022-05-26 20:30:28 +0800

    搜索组件

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-8 leading-8 my-4">
+    <input
+      v-model.trim="name"
+      type="text"
+      h="full"
+      p="x-2"
+      bg="gray-100"
+      text="gray-600"
+      outline="none"
+      @keydown.enter="go"
+    />
+    <button
+      class="rounded"
+      h="full"
+      m="l-4"
+      p="x-2"
+      bg="green-500"
+      text="white lg"
+      @click="go"
+    >
+      Go <IconMdiGoogleDownasaur class="animate-bounce align-middle" />
+    </button>
+  </div>
+</template>
