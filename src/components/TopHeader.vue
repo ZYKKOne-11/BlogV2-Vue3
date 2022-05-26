5284fd3ac44ac802592bdcafc332172f9f22ba9d
LZY_Shrio <170280176@qq.com>
2022-05-26 20:30:13 +0800

    页首

---
+++ src/components/TopHeader.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+defineProps({
+  size: Boolean,
+})
+</script>
+
+<template>
+  <header class="flex justify-between">
+    <a v-if="!size" class="title" href="/">NoPatience</a>
+    <a v-if="size" class="smallTitle" href="/">NoPatience</a>
+    <DarkToggle />
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 2rem;
+  font-weight: bolder;
+}
+.smallTitle {
+  font-size: 1.5rem;
+  font-weight: bolder;
+  color: #FFA7C4;
+}
+</style>
