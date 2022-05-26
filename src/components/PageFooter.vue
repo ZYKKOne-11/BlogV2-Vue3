3bf47851db11a33fe56a97e2e9bbaedd58459db7
LZY_Shrio <170280176@qq.com>
2022-05-26 20:30:06 +0800

    页脚

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -4,8 +4,33 @@ defineProps({
 })
 </script>
 <template>
-  <footer class="fixed bottom-0" w="full" text="center" p="y-2">
-    <DarkToggle />
-    <div class="mx-auto opacity-25 text-sm">[{{ name }} Layout]</div>
+  <footer class="bottom-0" w="full" text="center" p="y-2">
+    <a class="link blue" href="/">NoPatience</a> · <a class="link green" href="#">KKO</a> ·
+    <a class="link pink" target="_blank" href="https://github.com/ZYKKOne-11/">GitHub
+    </a>
   </footer>
 </template>
+
+<style lang="scss" scoped>
+.link {
+   font-family: 'Merriweather', 'Georgia', serif;
+    font-weight: 400;
+    word-wrap: break-word;
+    font-kerning: normal;
+    -moz-font-feature-settings: "kern", "liga", "clig", "calt";
+    -ms-font-feature-settings: "kern", "liga", "clig", "calt";
+    -webkit-font-feature-settings: "kern", "liga", "clig", "calt";
+    font-feature-settings: "kern", "liga", "clig", "calt";
+}
+.blue:hover {
+  color: #70a1ff;
+}
+
+.pink:hover {
+  color: #ff6b81;
+}
+
+.green:hover {
+  color: #2ed573;
+}
+</style>
