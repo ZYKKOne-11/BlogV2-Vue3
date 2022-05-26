3aa7697bb0bf8227aa0841009adabd9a95d54f96
LZY_Shrio <170280176@qq.com>
2022-05-26 20:30:01 +0800

    个人信息组件

---
+++ src/components/InfoLable.vue
@@ -0,0 +1,31 @@
+<template>
+  <aside class="my-4 flex">
+    <div class="w-14 rounded-full">
+      <img class="w-14 rounded-full" src="@/assets/avaimg.png" alt="" />
+    </div>
+    <div class="info">
+      <span>Personal blog by <span style="color: #ff6b81"> ZYKKOne.</span></span>
+      <span>" a Funny Coder "（ 也许是个艺术家或肥宅,</span>
+    </div>
+  </aside>
+</template>
+
+<style lang="scss" scoped>
+.info {
+  text-align: left;
+  margin-left: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  span {
+    font-family: 'Merriweather', 'Georgia', serif;
+    font-weight: 400;
+    word-wrap: break-word;
+    font-kerning: normal;
+    -moz-font-feature-settings: "kern", "liga", "clig", "calt";
+    -ms-font-feature-settings: "kern", "liga", "clig", "calt";
+    -webkit-font-feature-settings: "kern", "liga", "clig", "calt";
+    font-feature-settings: "kern", "liga", "clig", "calt";
+  }
+}
+</style>
