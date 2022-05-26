bda04ab8d832e6371d54c970ad0551cffdce34f2
LZY_Shrio <170280176@qq.com>
2022-05-26 20:30:39 +0800

    主页静态结构

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,38 +1,85 @@
 <script lang="ts" setup>
-const name = ref('')
 const router = useRouter()
 
-function go() {
-  if (name.value) router.push(`/hello/${encodeURIComponent(name.value)}`)
+function go(value: string) {
+  if (value) router.push(`/hello/${encodeURIComponent(value)}`)
 }
 </script>
 <template>
-  <div>
-    <img class="mx-auto" src="@/assets/logo.png" alt="" />
-    <div class="my-4">Index View <IconCustomFace /></div>
+  <div class="wrap">
+    <TopHeader />
+    <InfoLable />
+    <!-- <div class="my-4">Index View <IconCustomFace /></div> -->
+    <main>
+      <article>
+        <header>
+          <h3 @click="go('测试标题嘎嘎长的标题！！！！！！！')">测试标题嘎嘎长的标题！！！！！！！</h3>
+          <small>星期五，21日，7月，2021 · ❤️</small>
+        </header>
+        <p>
+          让我把代码变的整洁吧！～让我把代码变的整洁吧！～让我把代码变的整洁吧！～让我把代码变的整洁吧！～让我把代码变的整洁吧！～
+        </p>
+      </article>
+      <article>
+        <header><h3>测试标题嘎嘎长的标题！</h3></header>
+        <small>星期五，21日，7月，2021 · ❤️</small>
+        <p>让我把代码变的整洁吧！～</p>
+      </article>
+      <article>
+        <header><h3>测试标题嘎嘎长的标题！！！</h3></header>
+        <small>星期五，21日，7月，2021 · ❤️</small>
+        <p>让我把代码变的整洁吧！～</p>
+      </article>
+    </main>
     <BasicHello />
-    <div class="h-8 leading-8 my-4">
-      <input
-        v-model.trim="name"
-        type="text"
-        h="full"
-        p="x-2"
-        bg="gray-100"
-        text="gray-600"
-        outline="none"
-        @keydown.enter="go"
-      />
-      <button
-        class="rounded"
-        h="full"
-        m="l-4"
-        p="x-2"
-        bg="green-500"
-        text="white lg"
-        @click="go"
-      >
-        Go <IconMdiGoogleDownasaur class="animate-bounce align-middle" />
-      </button>
-    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.wrap {
+  max-width: 42rem;
+  margin-left: auto;
+  margin-right: auto;
+  main {
+    margin-top: 5rem;
+    article {
+      margin-bottom: 4rem;
+      text-align: left;
+      header {
+        margin-bottom: 0.2rem;
+        h3 {
+          color: #d23669;
+          font-size: 1.75rem;
+          font-weight: bolder;
+          letter-spacing: 0.1rem;
+          text-rendering: optimizeLegibility;
+          font-size: 1.4427rem;
+          line-height: 1.1;
+          cursor: pointer;
+          margin-bottom: 1rem;
+        }
+        small {
+          font-family: 'Merriweather', 'Georgia', serif;
+          font-weight: 400;
+          word-wrap: break-word;
+          font-kerning: normal;
+          -moz-font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+          -ms-font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+          -webkit-font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+          font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+        }
+      }
+      p {
+        font-family: 'Merriweather', 'Georgia', serif;
+        font-weight: 400;
+        word-wrap: break-word;
+        font-kerning: normal;
+        -moz-font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+        -ms-font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+        -webkit-font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+        font-feature-settings: 'kern', 'liga', 'clig', 'calt';
+      }
+    }
+  }
+}
+</style>
