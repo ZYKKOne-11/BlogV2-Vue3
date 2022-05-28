e89a683a3203d85b186ee2ba85b7f57e20e26ed1
LZY_Shrio <170280176@qq.com>
2022-05-28 12:01:15 +0800

    404修改

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -2,10 +2,11 @@
   <div class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
     <router-view />
     <div class="mt-20">
-      <router-link to="/" class="bg-green-400 px-4 py-2 text-white">
-        返回首页
+      <router-link to="/">
+        Go Back
       </router-link>
-      <PageFooter name="NotFound" class="mt-5" />
+
+      <PageFooter name="NotFound" class="fixed bottom-0 mt-5" />
     </div>
   </div>
 </template>
