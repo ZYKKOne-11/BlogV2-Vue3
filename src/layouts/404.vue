61b5decbd51fe844cefd9669fd0f3f883f9edd2e
LZY_Shrio <170280176@qq.com>
2022-05-30 13:59:47 +0800

    title

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
     <router-view />
+
     <div class="mt-20">
-      <router-link to="/">
-        Go Back
-      </router-link>
+      <router-link to="/"> Go Back </router-link>
 
       <PageFooter name="NotFound" class="fixed bottom-0 mt-5" />
     </div>
